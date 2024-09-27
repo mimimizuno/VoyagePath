cb7fbab2c77cd8345dd8e463f49467c859edc5de
mimimizuno <sukfishdry1217@gmail.com>
2024-09-27 17:55:48 +0900

    Add signup integration test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,18 @@ class UsersController < ApplicationController
   end
   
   def new
+    @user = User.new
   end
 
   def create
+    # ストロングパラメータを指定
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to VoyagePath!!"
+      redirect_to @user
+    else
+      render 'new', status: :unprocessable_entity
+    end
   end
 
   def show
@@ -23,4 +32,9 @@ class UsersController < ApplicationController
   def destroy
   end
 
+  private
+
+    def user_params
+      params.require(:user).permit(:user_name, :email, :password, :password_confirmation)
+    end
 end
