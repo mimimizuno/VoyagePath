b5d72463d0dab4a10290b491abfc17bd818f9278
mimimizuno <sukfishdry1217@gmail.com>
2024-09-28 16:23:00 +0900

    Add login and logout functions

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,17 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+// メニュー操作
+
+// トグルリスナーを追加してクリックをリッスンする
+document.addEventListener("turbo:load", function() {
+    let account = document.querySelector("#account");
+    if (account) {
+      account.addEventListener("click", function(event) {
+        event.preventDefault();
+        let menu = document.querySelector("#dropdown-menu");
+        menu.classList.toggle("active");
+      });
+    }
+  });
