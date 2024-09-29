a9156579c6470ccc1979d4f6ffaa28e8a4f061fa
mimimizuno <sukfishdry1217@gmail.com>
2024-09-29 16:09:51 +0900

    Add tasks model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_29_063342) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_29_070355) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "description"
+    t.date "due_date", null: false
+    t.jsonb "repetition"
+    t.boolean "completed", default: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "user_name"
@@ -28,4 +40,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_29_063342) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "tasks", "users"
 end
