d524d9f77a7fd554cb1fe614dd4c23fa4d9a96f6
mimimizuno <sukfishdry1217@gmail.com>
2024-10-03 06:27:20 +0900

    Add when user logins calc experience

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,6 +10,15 @@ class SessionsController < ApplicationController
       # ログイン時のチェックボックスで三項演算
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       log_in user
+      # ログイン前の経験値を保存
+      initial_experience_points = user.experience_points
+      # ログイン時に経験値を計算する
+      user.calculate_experience_on_login
+      # debugger
+      user.save
+      if user.experience_points > initial_experience_points
+        flash.now[:notice] = "Welcome back! Your experience points have been updated."
+      end
       redirect_to forwarding_url || user
     else
       flash.now[:danger] = 'Invalid email/password combination'
